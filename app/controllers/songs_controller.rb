f1b125ffc23a205ebff87f3570e785bb18bd8bad
Colleen <colleenrust524@EMAIL.com>
2023-03-31 13:09:04 -0500

    application controller to disable rails protection

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,21 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    render :index
+  end
+  def show
+    @song = Song.find_by(id: params[:id])
+    render :show
+  end
+  def create
+    @song = Song.new(
+      title: params[:title]
+      artist: params [:artist]
+      album: params [:album]
+      year: params [:year])
+    end
+
+    song.save
+    render :show
+  end
+end
